9daee84ce53b045284ed050bbe086db7b178b59e
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2025-01-15 14:22:01 +0530

    Update W-GAN.py

---
+++ W-GAN.py
@@ -0,0 +1,152 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+from torchvision.utils import save_image
+
+import numpy as np
+import datetime
+import os, sys
+
+from matplotlib.pyplot import imshow, imsave
+
+MODEL_NAME = 'W-GAN'
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def to_onehot(x, num_classes=10):
+    assert isinstance(x, int) or isinstance(x, (torch.LongTensor, torch.cuda.LongTensor))
+    if isinstance(x, int):
+        c = torch.zeros(1, num_classes).long()
+        c[0][x] = 1
+    else:
+        x = x.cpu()
+        c = torch.LongTensor(x.size(0), num_classes)
+        c.zero_()
+        c.scatter_(1, x, 1) # dim, index, src value
+    return c
+
+def get_sample_image(G, n_noise=100):
+    """
+        save sample 100 images
+    """
+    img = np.zeros([280, 280])
+    for j in range(10):
+        c = torch.zeros([10, 10]).to(DEVICE)
+        c[:, j] = 1
+        z = torch.randn(10, n_noise).to(DEVICE)
+        y_hat = G(z,c).view(10, 28, 28)
+        result = y_hat.cpu().data.numpy()
+        img[j*28:(j+1)*28] = np.concatenate([x for x in result], axis=-1)
+    return img
+
+class Discriminator(nn.Module):
+    """
+        Convolutional Discriminator for MNIST
+    """
+    def __init__(self, in_channel=1, input_size=784, condition_size=10, num_classes=1):
+        super(Discriminator, self).__init__()
+        self.transform = nn.Sequential(
+            nn.Linear(input_size+condition_size, 784),
+            nn.LeakyReLU(0.2),
+        )
+        self.conv = nn.Sequential(
+            # 28 -> 14
+            nn.Conv2d(in_channel, 512, 3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+            # 14 -> 7
+            nn.Conv2d(512, 256, 3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+            # 7 -> 4
+            nn.Conv2d(256, 128, 3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+            nn.AvgPool2d(4),
+        )
+        self.fc = nn.Sequential(
+            # reshape input, 128 -> 1
+            nn.Linear(128, 1),
+        )
+    
+    def forward(self, x, c=None):
+        # x: (N, 1, 28, 28), c: (N, 10)
+        x, c = x.view(x.size(0), -1), c.float() # may not need
+        v = torch.cat((x, c), 1) # v: (N, 794)
+        y_ = self.transform(v) # (N, 784)
+        y_ = y_.view(y_.shape[0], 1, 28, 28) # (N, 1, 28, 28)
+        y_ = self.conv(y_)
+        y_ = y_.view(y_.size(0), -1)
+        y_ = self.fc(y_)
+        return y_
+    
+
+class Generator(nn.Module):
+    """
+        Convolutional Generator for MNIST
+    """
+    def __init__(self, input_size=100, condition_size=10):
+        super(Generator, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(input_size+condition_size, 4*4*512),
+            nn.ReLU(),
+        )
+        self.conv = nn.Sequential(
+            # input: 4 by 4, output: 7 by 7
+            nn.ConvTranspose2d(512, 256, 3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            # input: 7 by 7, output: 14 by 14
+            nn.ConvTranspose2d(256, 128, 4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            # input: 14 by 14, output: 28 by 28
+            nn.ConvTranspose2d(128, 1, 4, stride=2, padding=1, bias=False),
+            nn.Tanh(),
+        )
+        
+    def forward(self, x, c):
+        # x: (N, 100), c: (N, 10)
+        x, c = x.view(x.size(0), -1), c.float() # may not need
+        v = torch.cat((x, c), 1) # v: (N, 110)
+        y_ = self.fc(v)
+        y_ = y_.view(y_.size(0), 512, 4, 4)
+        y_ = self.conv(y_) # (N, 28, 28)
+        return y_
+    
+D = Discriminator().to(DEVICE)
+G = Generator().to(DEVICE)
+# D.load_state_dict('D_dc.pkl')
+# G.load_state_dict('G_dc.pkl')
+
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize(mean=[0.5],
+                                std=[0.5])]
+)
+
+mnist = datasets.MNIST(root='../data/', train=True, transform=transform, download=True)
+
+batch_size = 64
+
+data_loader = DataLoader(dataset=mnist, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=True)
+
+D_opt = torch.optim.RMSprop(D.parameters(), lr=0.0005)
+G_opt = torch.optim.RMSprop(G.parameters(), lr=0.0005)
+
+max_epoch = 30 # need more than 100 epochs for training generator
+step = 0
+g_step = 0
+n_noise = 100
+
+def n_critic(step, nc=2):
+    if step < 25 or step % 500 == 0:
+        return 100
+    return nc
+
+D_labels = torch.ones([batch_size, 1]).to(DEVICE) # Discriminator Label to real
+D_fakes = torch.zeros([batch_size, 1]).to(DEVICE) # Discriminator Label to fake
+
